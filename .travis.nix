9c9ef7d5f8697e2d48c03be9de8c8357777bd6ba
Zhenya Vinogradov <zhenyavinogradov@gmail.com>
2019-11-29 16:58:42 +0300

    Remove obsolete test, add a simple test for buildNpmPackage

--- .travis.nix
+++ .travis.nix
@@ -1,14 +1,8 @@
-# TODO:
-# * replace w/ more generic test
-# * also test npm packages
-# * unhide badge
-
-{ pkgs ? import <nixpkgs> {} }:
 let
-  bp = pkgs.callPackage ./default.nix {};
+  # we pin nixpkgs revision for tests
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs {};
+
+  npm-buildpackage = pkgs.callPackage ./default.nix {};
 in
-  import (fetchGit {
-    url = "https://github.com/obfusk/nix-vault-with-ui.git";
-    rev = "664d3d9a1f30626d0277a62c520cf82df0e0a2a1";
-    ref = "664d3d9-tag-you-are-it"; # TODO: "v0.1.0"
-  }) { inherit pkgs; npm-buildpackage = bp; }
+  import ./tests/buildNpmPackage { inherit pkgs npm-buildpackage; }
