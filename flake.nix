abde678d1584af0ad00477486bca26c880963a70
Alexander Bantyev <balsoft@balsoft.ru>
2020-09-03 11:36:21 +0300

    Fix flake overlay

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "Build npm and yarn packages";
 
   outputs = { self }: {
-    overlay = final: prev: final.callPackage ./. { };
+    overlay = final: prev: {
+      inherit (final.callPackage ./. { }) mkNodeModules buildNpmPackage buildYarnPackage;
+    };
   };
 }
