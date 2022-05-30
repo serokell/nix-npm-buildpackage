18331cd6c2f2659ac3ad355ae0bb3c24dc07b5bd
Yorick van Pelt <yorick@yorickvanpelt.nl>
2022-05-30 15:05:48 +0200

    Move from niv and travis to nix flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,20 @@
 {
   description = "Build npm and yarn packages";
 
-  outputs = { self }: {
-    overlay = final: prev: {
+  outputs = { self, nixpkgs }: {
+    legacyPackages.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
+    overlays.default = final: prev: {
       inherit (final.callPackage ./. { }) mkNodeModules buildNpmPackage buildYarnPackage;
+    };
+    checks.x86_64-linux = {
+      npm = import ./tests/buildNpmPackage {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        npm-buildpackage = self.legacyPackages.x86_64-linux;
+      };
+      yarn = import ./tests/buildNpmPackage/yarn.nix {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        npm-buildpackage = self.legacyPackages.x86_64-linux;
+      };
     };
   };
 }
