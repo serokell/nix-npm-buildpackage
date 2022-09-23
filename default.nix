530ce2db5c3940250e32107a5044865b1e5fe71c
Hlöðver Sigurðsson <hlolli@gmail.com>
2022-09-23 14:31:27 +0200

    feat(buildNpmPackage): add force flag to copy cache operation

--- default.nix
+++ default.nix
@@ -206,7 +206,8 @@ in rec {
         export HOME=$(mktemp -d)
         chmod a-w "$HOME"
         # npm prune actually installs some packages sometimes
-        cp -RL --no-preserve=mode "${nodeModules}/npm-cache" "$PWD/npm-cache"
+        cp -fR --no-preserve=mode "${nodeModules}/npm-cache" "$PWD/npm-cache"
+
         export npm_config_cache="$PWD/npm-cache"
 
         if [[ -e ./node_modules ]]; then
