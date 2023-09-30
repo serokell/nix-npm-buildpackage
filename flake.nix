b87b255f30a6dc662bde1fc6d2dbba064ef58fee
Smaug123 <patrick+github@patrickstevens.co.uk>
2023-10-01 00:34:25 +0100

    Add Darwin support

--- flake.nix
+++ flake.nix
@@ -1,30 +1,43 @@
 {
   description = "Build npm and yarn packages";
 
-  outputs = { self, nixpkgs }: {
-    legacyPackages.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.callPackage ./default.nix {};
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    systems = ["x86_64-linux" "aarch64-darwin"];
+  in {
+    legacyPackages = builtins.listToAttrs (builtins.map (system: {
+        name = system;
+        value = nixpkgs.legacyPackages.${system}.callPackage ./default.nix {};
+      })
+      systems);
     overlays.default = final: prev: {
-      inherit (final.callPackage ./. { }) mkNodeModules buildNpmPackage buildYarnPackage;
+      inherit (final.callPackage ./. {}) mkNodeModules buildNpmPackage buildYarnPackage;
     };
-    checks.x86_64-linux = let
-      nixpkgs' = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      npm6 = import ./tests/buildNpmPackage {
-        pkgs = nixpkgs';
-        npm-buildpackage = self.legacyPackages.x86_64-linux.override {
-          nodejs = nixpkgs'.nodejs-14_x;
+    checks = builtins.listToAttrs (builtins.map (system: {
+        name = system;
+        value = let
+          nixpkgs' = nixpkgs.legacyPackages.${system};
+        in {
+          npm6 = import ./tests/buildNpmPackage {
+            pkgs = nixpkgs';
+            npm-buildpackage = self.legacyPackages.${system}.override {
+              nodejs = nixpkgs'.nodejs-14_x;
+            };
+          };
+          npm8 = import ./tests/buildNpmPackage {
+            pkgs = nixpkgs';
+            npm-buildpackage = self.legacyPackages.${system}.override {
+              nodejs = nixpkgs'.nodejs-18_x;
+            };
+          };
+          yarn = import ./tests/buildYarnPackage {
+            pkgs = nixpkgs.legacyPackages.${system};
+            npm-buildpackage = self.legacyPackages.${system};
+          };
         };
-      };
-      npm8 = import ./tests/buildNpmPackage {
-        pkgs = nixpkgs';
-        npm-buildpackage = self.legacyPackages.x86_64-linux.override {
-          nodejs = nixpkgs'.nodejs-18_x;
-        };
-      };
-      yarn = import ./tests/buildYarnPackage {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        npm-buildpackage = self.legacyPackages.x86_64-linux;
-      };
-    };
+      })
+      systems);
   };
 }
